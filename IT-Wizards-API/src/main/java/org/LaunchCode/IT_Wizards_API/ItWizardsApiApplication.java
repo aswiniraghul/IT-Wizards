7840578566a54604593ed48694b0f9f93629fa5d
joshualjames <142367770+joshualjames@users.noreply.github.com>
2024-06-29 12:57:46 -0500

    Completed Initial Setup

---
+++ IT-Wizards-API/src/main/java/org/LaunchCode/IT_Wizards_API/ItWizardsApiApplication.java
@@ -0,0 +1,13 @@
+package org.LaunchCode.IT_Wizards_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItWizardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItWizardsApiApplication.class, args);
+	}
+
+}
---
+++ IT-Wizards-API/src/test/java/org/LaunchCode/IT_Wizards_API/ItWizardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.LaunchCode.IT_Wizards_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItWizardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
