0f59446d8a2ae04741ab3c0f1ca95d55b8813d11
Joshua James <josh452063@gmail.com>
2024-07-16 09:00:23 -0500

    Added CorsConfig and updated ItemController to utilize http://localhost8080 instead of *.

--- IT-Wizards-API/src/main/java/org/LaunchCode/IT_Wizards_API/controllers/ItemController.java
+++ IT-Wizards-API/src/main/java/org/LaunchCode/IT_Wizards_API/controllers/ItemController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin("http://localhost8080")
 public class ItemController {
     @Autowired
     private ItemRepository itemRepository;
