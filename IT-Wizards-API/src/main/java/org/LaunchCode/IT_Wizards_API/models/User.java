4905b61e42640f3d606f3cc658932b25320f1f60
Aswini Raghul <aswiniraghul@gmail.com>
2024-07-21 17:28:18 -0500

    Added User model

---
+++ IT-Wizards-API/src/main/java/org/LaunchCode/IT_Wizards_API/models/User.java
@@ -0,0 +1,38 @@
+package org.LaunchCode.IT_Wizards_API.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", nullable = false, unique = true)
+    private String userName;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "user_password", nullable = false)
+    private String userPassword;
+
+    @Column(name = "mail_id")
+    private String mailId;
+
+    @Column(name = "login_role", nullable = false)
+    private String loginRole;
+}
