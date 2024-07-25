5614c450415f40994ed1950871cd36758c42ce08
Joshua James <josh452063@gmail.com>
2024-07-25 10:31:10 -0500

    removed data folder from API that kept regenerating when I merged.

--- IT-Wizards-API/src/main/java/org/LaunchCode/IT_Wizards_API/controllers/HomeController.java
+++ IT-Wizards-API/src/main/java/org/LaunchCode/IT_Wizards_API/controllers/HomeController.java
@@ -4,10 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-
-
-
 @RestController
 @RequestMapping("/home")
 public class HomeController {
