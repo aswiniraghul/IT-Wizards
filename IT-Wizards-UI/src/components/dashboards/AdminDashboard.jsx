27ef75fb5a5ec4882f1e16793189f1ffb40fd76e
Aswini Raghul <aswiniraghul@gmail.com>
2024-07-25 00:53:56 -0500

    Added separate landing pages for admin and user

---
+++ IT-Wizards-UI/src/components/dashboards/AdminDashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      <h3>Logged in as Admin. Welcome to Admin dashboard !!!</h3>
+    </div>
+  );
+};
+
+export default AdminDashboard;
