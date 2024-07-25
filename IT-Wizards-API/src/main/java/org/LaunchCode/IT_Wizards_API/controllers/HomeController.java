2f96605c5043f906a22bf82a7f53615c9795918f
Josh James <142367770+joshualjames@users.noreply.github.com>
2024-07-25 10:19:01 -0500

    Update HomeController.java
    Accidentally took this import out

--- IT-Wizards-API/src/main/java/org/LaunchCode/IT_Wizards_API/controllers/HomeController.java
+++ IT-Wizards-API/src/main/java/org/LaunchCode/IT_Wizards_API/controllers/HomeController.java
@@ -1,7 +1,7 @@
 package org.LaunchCode.IT_Wizards_API.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
