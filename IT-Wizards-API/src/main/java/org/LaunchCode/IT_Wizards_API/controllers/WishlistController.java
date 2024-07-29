26aecef2b411e773495d87d654123d5f1e475811
frazier0326 <frazier0326@gmail.com>
2024-07-28 21:41:09 -0500

    removed duplicate path names in controllers

--- IT-Wizards-API/src/main/java/org/LaunchCode/IT_Wizards_API/controllers/WishlistController.java
+++ IT-Wizards-API/src/main/java/org/LaunchCode/IT_Wizards_API/controllers/WishlistController.java
@@ -15,16 +15,17 @@ public class WishlistController {
     @Autowired
     private WishlistRepository wishlistRepository;
 
-    @PostMapping("/newWishlist")
+    @PostMapping()
     Wishlist newWishlist(@RequestBody Wishlist newWishlist) {
         return wishlistRepository.save(newWishlist);
     }
+
     @GetMapping()
     List<Wishlist> getAllWishlists() {
         return wishlistRepository.findAll();
     }
 
-    @GetMapping("/wishlist/{id}")
+    @GetMapping("/{id}")
     Wishlist getWishlistById(@PathVariable Long id) {
         return wishlistRepository.findById(id)
                 .orElseThrow(() -> new WishlistNotFoundException(id));
