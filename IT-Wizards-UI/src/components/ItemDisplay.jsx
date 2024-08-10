2e1d0f25be8fc0b63dee7789e9efeb63bb263f2e
Joshua James <josh452063@gmail.com>
2024-08-10 14:03:09 -0500

    added a text display for when there are no search results.

--- IT-Wizards-UI/src/components/ItemDisplay.jsx
+++ IT-Wizards-UI/src/components/ItemDisplay.jsx
@@ -21,12 +21,16 @@ const ItemDisplay = ({ searchTerm }) => {
     }
   };
 
+  const itemsArr = [];
+
   return (
     <section className="bg-blue-500 w-full border-t-4 border-b-4 border-black overflow-y-auto">
       <section className="bg-purple-400">
         <div className="container bg-purple-400 py-8 px-12">
           <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
-            <h2 className="text-5xl text-center font-bold underline mb-2">Welcome to the Shop</h2>
+            <h2 className="text-5xl text-center font-bold underline mb-2">
+              Welcome to the Shop
+            </h2>
 
             <div className="container m-auto max-w-5xl py-12">
               <div className="table-fixed border-separate border-spacing-6 border text-left border-purple-600">
@@ -40,59 +44,76 @@ const ItemDisplay = ({ searchTerm }) => {
                         .includes(searchTerm.toLowerCase())
                     ) {
                       return;
+                    } else {
+                      itemsArr.push(item);
+                      console.log(itemsArr.length);
                     }
+
                     return (
-                      <div className="mb-2 ml-2 mr-2 hover:scale-105" key={item.id}>
-                        <Link to={`/items/${item.id}`}>
-                          <img src={cauldron} className="size-72"></img>
-                        </Link>
+                      <div>
+                        <div
+                          className="mb-2 ml-2 mr-2 hover:scale-105"
+                          key={item.id}
+                        >
+                          <Link to={`/items/${item.id}`}>
+                            <img src={cauldron} className="size-72"></img>
+                          </Link>
 
-                        <div className="flex items-center justify-center">
-                          {item.name}
-                        </div>
-                        <div className="flex items-center justify-center">
-                          ${(Math.round(item.price * 100) / 100).toFixed(2)}
-                        </div>
-
-                        {cart.getItemQuantity(item.id) > 0 ? (
-                          <div className="">
-                            <div className="flex bg-indigo-600  text-white text-sm font-bold py-2 px-4 ml-3 mr-3 rounded-full w-fit  mt-5  focus:outline-none focus:shadow-outline">
-                              In Cart: {cart.getItemQuantity(item.id)}
-                              <button
-                                onClick={() => cart.addOneToCart(item)}
-                                className=" mx-2 align-bottom bg-green-500 text-slate-700 text-sm font-bold rounded-full w-8 h-min"
-                              >
-                                +
-                              </button>
-                              <button
-                                onClick={() => cart.removeOneFromCart(item)}
-                                className="size-20 mx-2 align-bottom bg-red-500  text-slate-700 text-sm font-bold rounded-full w-8 h-min"
-                              >
-                                -
-                              </button>
-                            </div>
-                            <div></div>
-                            <div>
-                              <button
-                                className="flex bg-red-600 hover:bg-red-700 ml-3 mr-3  text-white text-sm font-bold py-2 px-4 rounded-full w-fit mt-3 mb-3 focus:outline-none focus:shadow-outline"
-                                onClick={() => cart.deleteFromCart(item)}
-                              >
-                                Remove all from cart
-                              </button>
-                            </div>
+                          <div className="flex items-center justify-center">
+                            {item.name}
                           </div>
-                        ) : (
-                          <button
-                            onClick={() => cart.addOneToCart(item)}
-                            className="bg-indigo-600 hover:bg-indigo-700   text-white font-bold py-2 px-4 rounded-full w-full mt-6 hover:text-green-600 focus:outline-none focus:shadow-outline"
-                            type="submit"
-                          >
-                            Add to Cart
-                          </button>
-                        )}
+                          <div className="flex items-center justify-center">
+                            ${(Math.round(item.price * 100) / 100).toFixed(2)}
+                          </div>
+
+                          {cart.getItemQuantity(item.id) > 0 ? (
+                            <div className="">
+                              <div className="flex bg-indigo-600  text-white text-sm font-bold py-2 px-4 ml-3 mr-3 rounded-full w-fit  mt-5  focus:outline-none focus:shadow-outline">
+                                In Cart: {cart.getItemQuantity(item.id)}
+                                <button
+                                  onClick={() => cart.addOneToCart(item)}
+                                  className=" mx-2 align-bottom bg-green-500 text-slate-700 text-sm font-bold rounded-full w-8 h-min"
+                                >
+                                  +
+                                </button>
+                                <button
+                                  onClick={() => cart.removeOneFromCart(item)}
+                                  className="size-20 mx-2 align-bottom bg-red-500  text-slate-700 text-sm font-bold rounded-full w-8 h-min"
+                                >
+                                  -
+                                </button>
+                              </div>
+                              <div></div>
+                              <div>
+                                <button
+                                  className="flex bg-red-600 hover:bg-red-700 ml-3 mr-3  text-white text-sm font-bold py-2 px-4 rounded-full w-fit mt-3 mb-3 focus:outline-none focus:shadow-outline"
+                                  onClick={() => cart.deleteFromCart(item)}
+                                >
+                                  Remove all from cart
+                                </button>
+                              </div>
+                            </div>
+                          ) : (
+                            <button
+                              onClick={() => cart.addOneToCart(item)}
+                              className="bg-indigo-600 hover:bg-indigo-700   text-white font-bold py-2 px-4 rounded-full w-full mt-6 hover:text-green-600 focus:outline-none focus:shadow-outline"
+                              type="submit"
+                            >
+                              Add to Cart
+                            </button>
+                          )}
+                        </div>
                       </div>
                     );
                   })}
+                  {itemsArr.length === 0 && (
+                    <>
+                      <div></div>
+                      <div className="flex-col text-2xl font-bold mt-6 text-green-500 text-center">
+                        No items with the given search. Try a new search!
+                      </div>
+                    </>
+                  )}
                 </div>
               </div>
             </div>
